c87f54df3cf119ed4fb2e175999caa9ab394c769
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2020-08-24 16:26:14 -0500

    CORS added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const UserSchema = require('./models/userSchema');
 const BicycleSchema = require('./models/bicycleSchema');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect(
